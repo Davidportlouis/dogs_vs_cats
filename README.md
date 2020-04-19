4f30f206015030a19bd53822163adfd9d54c269a
David Port Louis <dportlouis@gmail.com>
2020-04-19 20:31:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Using Pytorch for Kaggle's Dog vs Cat Classifier
+
+![Cat vs Dog](./static/cat_dog.jpeg)
+
+* Organizing Train/Validation data  [Refer preprocess.py](preprocess.py)
+* Transfer Learning
+* Training and save the model
+* Inference using test 
+
+### Important Note
+> This model was trained using only 1000 images.
+
+> 500 cat and 500 dog images for training.
+
+> another 250 cat and 250 dog images for validation.
+
+> Trained on intel core i5-4460 w/o cuda.
+
+> Training time for 1 epoch: 483.96 sec {8:30 min:sec}.
+
+> Testing acc: 94% after 1 epoch
---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request
+from PIL import Image
+import torch
+from torch import nn
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import os
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './static/uploads/'
+
+model = torch.load('checkpoint.pth')
+
+def apply_test_transforms(inp):
+    out = transforms.functional.resize(inp, [224,224])
+    out = transforms.functional.to_tensor(out)
+    out = transforms.functional.normalize(out, [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    return out
+
+def pred_class(image):
+    img = Image.open(image)
+    img_tensor = apply_test_transforms(img)
+    softMax = nn.Softmax(dim = 1)
+    batch = torch.stack([img_tensor])
+    preds = softMax(model(batch))
+    return (preds[0,0].item(),preds[0,1].item())
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        f = request.files['image']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+        predict = pred_class('./static/uploads/' + f.filename)
+        return render_template('predict.html',img=f.filename,cat=round(predict[0]*100,2),dog=round(predict[1]*100,2))
+
+if __name__ == "__main__":
+    app.run(debug=True)
